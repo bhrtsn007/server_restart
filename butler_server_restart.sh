c9e38e40bb1b8e1bb05c8ca8adcc7a83f26134a3
bhrtsn007 <bharat.s@greyorange.com>
2020-11-26 14:03:17 +0530

    PID validation check added

--- butler_server_restart.sh
+++ butler_server_restart.sh
@@ -22,15 +22,50 @@ if [ "$data_sanity" == "true" ] && [ "$data_domain" == "true" ]; then
 	echo "$name please type 'Yes' for confirming and 'No' for aborting the process:"
 	read confirmation
 	if [ "$confirmation" == "Yes" ]; then
+		echo "Stopping Butler server"
 		sudo systemctl stop butler_server.service
-		sleep 2
-		sudo systemctl start butler_server.service
-		sleep 2
+		echo "Checking active PID"
+		sleep 20
+		pid_check_1=`pgrep -af "butler_server -mode"`
+		echo "PID found: $pid_check_1"
+		if [ ! -n "$pid_check_1" ]; then
+			echo "Server Stop successfully, starting butler_server application"
+			sudo systemctl start butler_server.service
+			sleep 5
+			echo "Restart complete"
+		else
+			echo "Server not stopped properly, trying to kill active PID"
+			sudo pkill -f "butler_server -mode"
+			echo "Checking active PID again"
+			sleep 20
+			pid_check_2=`pgrep -af "butler_server -mode"`
+			echo "PID found: $pid_check_2"
+			if [ ! -n "$pid_check_2" ]; then
+				echo "Server stop successfully after clearing PID, starting butler_server application"
+				sudo systemctl start butler_server.service
+	            sleep 5
+				echo "Restart complete"
+			else
+				echo "Server not stopped after killing PID, killing butler_server PID forcefully"
+				sudo pkill -9 -f "butler_server -mode"
+				sleep 20
+				echo "forcefull kill complete, Checking active PID again"
+				pid_check_3=`pgrep -af "butler_server -mode"`
+				echo "PID found: $pid_check_3"
+				if [ ! -n "$pid_check_3" ]; then
+					echo "Server stop successfully after clearing PID forcefully, starting butler_server application"
+					sudo systemctl start butler_server.service
+					sleep 5
+					echo "Restart complete"
+				else
+					echo "Something still wrong, check all PID which are active and then start butler_server application manually"
+				fi
+			fi
+			
+		fi
 		echo "####################################################"
 		echo "Check logs for Butler server status"
 		echo "####################################################"
-		echo ""
-		echo "Restarting Done"
 		echo ""
 		echo "####################################################"
 		echo "Also Run After Butler Server Restart Script"
